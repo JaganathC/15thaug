00964ded42b72971a7e33570290143c164b1896e
CHOWDAM JAGANATH <123239098+JaganathC@users.noreply.github.com>
2023-08-07 19:41:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ for (let i = 0; i < numStars; i++) {
     star.style.animationDuration = `${Math.random() * 6 + 4}s`;
     starContainer.appendChild(star);
 }
+// script.js
+function toggleVideo(index) {
+    const videos = document.querySelectorAll('.video-item video');
+    const expandedVideo = document.querySelector('.expanded-video');
+
+    if (expandedVideo) {
+        expandedVideo.remove();
+        return;
+    }
+
+    const video = videos[index].cloneNode(true);
+    video.controls = true;
+
+    const expandedContainer = document.createElement('div');
+    expandedContainer.classList.add('expanded-video');
+    expandedContainer.appendChild(video);
+
+    document.body.appendChild(expandedContainer);
+}
